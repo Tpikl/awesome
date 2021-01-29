307b11f98b5163245d310a644e7bc8ea16b2d4e7
Tpikl <pickles.tayler@gmail.com>
2021-01-29 08:38:39 -0700

    Rounded corners.

--- rc.lua
+++ rc.lua
@@ -620,3 +620,11 @@ end)
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
+
+
+-- Rounded corners.
+client.connect_signal("manage", function(c)
+    c.shape = function(cr, w, h)
+        gears.shape.rounded_rect(cr, w, h, 3)
+    end
+end)
